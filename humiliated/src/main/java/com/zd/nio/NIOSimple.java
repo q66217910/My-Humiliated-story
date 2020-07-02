2dd7b60771f579bfa1b10a58185daf8a5b84124b
dezhang <dezhang@homeinns.com>
2020-07-02 13:31:31 +0800

    mysql

--- humiliated/src/main/java/com/zd/nio/NIOSimple.java
+++ humiliated/src/main/java/com/zd/nio/NIOSimple.java
@@ -2,14 +2,35 @@ package com.zd.nio;
 
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * nio 例子
  */
 public class NIOSimple {
 
-    public static void main(String[] args) {
-        System.out.println(UUID.randomUUID().toString());
-    }
+    static int i = 0;
 
+    public static void main(String[] args) throws InterruptedException {
+
+
+        ReentrantLock lock = new ReentrantLock();
+
+        for (int j = 0; j < 20000; j++) {
+            new Thread(() -> {
+                lock.lock();
+                i=i+1;
+                lock.unlock();
+            }).start();
+        }
+
+        for (int j = 0; j < 20000; j++) {
+            lock.lock();
+            i=i+1;
+            lock.unlock();
+        }
+
+        Thread.sleep(10000);
+        System.out.println(i);
+    }
 }
